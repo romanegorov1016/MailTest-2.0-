ff4af4b25d757eb3a8a46f1dfd8b319a1b43ff86
romanegorov1016 <36641944+romanegorov1016@users.noreply.github.com>
2018-10-31 12:44:22 +0300

    Add files via upload

---
+++ GmailTest/src/test/java/Utils/Listener.java
@@ -0,0 +1,41 @@
+package Utils;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+
+public class Listener implements ITestListener {
+
+    public void onFinish(ITestContext arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    public void onStart(ITestContext arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    public void onTestFailure(ITestResult result) {
+        Screenshoter.takeScreenshot();
+    }
+
+    private void File(String property) {
+        // TODO Auto-generated method stub
+    }
+
+    public void onTestSkipped(ITestResult arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    public void onTestStart(ITestResult arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    public void onTestSuccess(ITestResult arg0) {
+        // TODO Auto-generated method stub
+    }
+}
